80331d8a6e0e092d4f78bdd527200f5f3600edcf
CHARANRAJ PATTAR <charanrajpattar96@gmail.com>
2024-12-23 15:20:54 +0530

    updating code

---
+++ backend/models/admin.js
@@ -0,0 +1,19 @@
+// models/admin.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const AdminSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { type: String, default: 'admin' }, // Role can be "admin" or "superadmin"
+});
+
+// Hash password before saving
+AdminSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
